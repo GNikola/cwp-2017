109ae7148428520dbfe56eae5a5f6ca41beef407
GNikola <32159083+GNikola@users.noreply.github.com>
2017-11-24 10:46:51 +0100

    domasna1-javascript

---
+++ C2/Domasno/index.js
@@ -0,0 +1,20 @@
+function c2f(celsius){
+	console.log(celsius * (9/5) + 32)
+}
+
+c2f(30)
+c2f(45)
+c2f(110)
+c2f(220)
+c2f(7)
+
+
+function f2c(fahrenheit){
+	console.log((fahrenheit - 32) * 5 / 9)
+}
+
+f2c(120);
+f2c(70);
+f2c(220);
+f2c(118);
+f2c(96);
