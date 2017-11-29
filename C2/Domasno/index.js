1e9fdd38af0e0032c5c2f08b5dbaf92e0c637e5f
GNikola <32159083+GNikola@users.noreply.github.com>
2017-11-29 12:12:16 +0100

    Domasna1-JS-bolje_verzija

--- C2/Domasno/index.js
+++ C2/Domasno/index.js
@@ -20,24 +20,65 @@ f2c(118);
 f2c(-96);
 
 
-function temp1(celsius){
-	console.log(celsius + '°C' + ' > ' + (celsius * (9/5) + 32) + '°F')
+
+
+
+function temp(number, type){
+	if(type=='c2f'){
+		console.log('Celsius to Fahrenheit:' + ' ' + (number * (9/5) + 32) + '°F')
+	}
+	else if(type=='f2c'){
+		console.log('Fahrenheit to Celsius:' + ' ' + (number - 32) * 5 / 9 + '°C')
+	}
+	else('Vnesovte greshen podatok')
 }
 
-temp1(celsius=30, celsius * (9 / 5) + 32);
-temp1(celsius=-17, celsius * (9 / 5) + 32);
-temp1(celsius=110, celsius * (9 / 5) + 32);
-temp1(celsius=220, celsius * (9 / 5) + 32);
-temp1(celsius=-28, celsius * (9 / 5) + 32);
+
+temp(20, 'c2f');
+temp(30, 'f2c');
 
 
-function temp2(fahrenheit){
-	console.log(fahrenheit + '°F' + ' > ' + (fahrenheit - 32) * 5 / 9 + '°C')
-}
 
-temp2(fahrenheit=120, (fahrenheit - 32) * 5 / 9);
-temp2(fahrenheit=-20, (fahrenheit - 32) * 5 / 9);
-temp2(fahrenheit=220, (fahrenheit - 32) * 5 / 9);
-temp2(fahrenheit=118, (fahrenheit - 32) * 5 / 9);
-temp2(fahrenheit=-96, (fahrenheit - 32) * 5 / 9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function temp1(celsius){
+// 	console.log(celsius + '°C' + ' > ' + (celsius * (9/5) + 32) + '°F')
+// }
+
+// temp1(celsius=30, celsius * (9 / 5) + 32);
+// temp1(celsius=-17, celsius * (9 / 5) + 32);
+// temp1(celsius=110, celsius * (9 / 5) + 32);
+// temp1(celsius=220, celsius * (9 / 5) + 32);
+// temp1(celsius=-28, celsius * (9 / 5) + 32);
+
+
+// function temp2(fahrenheit){
+// 	console.log(fahrenheit + '°F' + ' > ' + (fahrenheit - 32) * 5 / 9 + '°C')
+// }
+
+// temp2(fahrenheit=120, (fahrenheit - 32) * 5 / 9);
+// temp2(fahrenheit=-20, (fahrenheit - 32) * 5 / 9);
+// temp2(fahrenheit=220, (fahrenheit - 32) * 5 / 9);
+// temp2(fahrenheit=118, (fahrenheit - 32) * 5 / 9);
+// temp2(fahrenheit=-96, (fahrenheit - 32) * 5 / 9);
 
